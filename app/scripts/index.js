b379d3120eca23dfa1ab2709560a2d3e6ba9d7cc
Eric Brengle <eric.brengle@gmail.com>
2015-02-25 17:29:42 -0600

    got x appending to the table

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,13 +1,20 @@
 'use strict';
 //Firebase URL
-var fb = new Firebase('https://tic-tac-toe-app.firebaseio.com/');
+//var fb = new Firebase('https://tic-tac-toe-app.firebaseio.com/');
 var playerIcon = 'X';
 
-$('tbody').on( "click", function(event){
-  $( ".span" ).html( event.target.className );
-  fb.push( event.target.className );
-  //var index = $('.item1').index( this );
-
-  //$('.item1').text( "This is" + index);
+$('tbody').on( "click", 'td', function(event){
+  //$( ".span" ).html( event.target.id );
+  console.log($(this));
+  $(this).append(playerIcon);
+  //fb.push( event.target.className );
+  //var i = event.target.attr('id');
+  //alert(i);
 
 });
+
+//$( "div" ).click(function() {
+  // `this` is the DOM element that was clicked
+  //var index = $( "div" ).index( this );
+  //$( "span" ).text( "That was div index #" + index );
+//});
