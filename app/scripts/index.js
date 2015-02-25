096e9b891a7391f6ce527087fc121de978d46e46
Eric Brengle <eric.brengle@gmail.com>
2015-02-25 14:10:28 -0600

    talking to firebase

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,10 +1,11 @@
 'use strict';
 //Firebase URL
 var fb = new Firebase('https://tic-tac-toe-app.firebaseio.com/');
+var playerIcon = 'X';
 
 $('tbody').on( "click", function(event){
-  $( ".span" ).html( "clicked: " + event.target.className );
-
+  $( ".span" ).html( event.target.className );
+  fb.push( event.target.className );
   //var index = $('.item1').index( this );
 
   //$('.item1').text( "This is" + index);
