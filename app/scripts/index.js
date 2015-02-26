e29fd9326145a49f0d4c056554649a42903f9318
Matthew Reuther <matthew.reuther@gmail.com>
2015-02-25 18:39:44 -0600

    commented out js to give us a better understanding

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -2,8 +2,10 @@
 //Firebase URL
 //var fb = new Firebase('https://tic-tac-toe-app.firebaseio.com/');
 
+//Set first player to X
 var currentPlayer = "X";
 
+//Funcation that allows player one to go first with an X then player two is an O
 function changePlayer(){
     if( currentPlayer  == "X" )
         currentPlayer = "O";
@@ -11,8 +13,9 @@ function changePlayer(){
         currentPlayer = "X";
 }
 
+//Created an event listener on click of Table TD that appeneds first player(currentplayer)
+//an X then flips to an O on next players turn and unbide allows players to not click in an clciked area already.
 $('td').on('click', function(event){
-    // This = current TD
     $(this).append( currentPlayer );
     $(this).unbind("click");
     changePlayer();
