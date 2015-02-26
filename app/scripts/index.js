ef676e0411f0e97d04a7dfa29211e841c6433983
Matthew Reuther <matthew.reuther@gmail.com>
2015-02-25 18:24:52 -0600

    ability to play single player application of tic tac toe

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,20 +1,20 @@
 'use strict';
 //Firebase URL
 //var fb = new Firebase('https://tic-tac-toe-app.firebaseio.com/');
-var playerIcon = 'X';
 
-$('tbody').on( "click", 'td', function(event){
-  //$( ".span" ).html( event.target.id );
-  console.log($(this));
-  $(this).append(playerIcon);
-  //fb.push( event.target.className );
-  //var i = event.target.attr('id');
-  //alert(i);
+var currentPlayer = "X";
 
+function changePlayer(){
+    if( currentPlayer  == "X" )
+        currentPlayer = "O";
+    else
+        currentPlayer = "X";
+}
+
+$('td').on('click', function(event){
+    // This = current TD
+    $(this).append( currentPlayer );
+    $(this).unbind("click");
+    changePlayer();
+    event.preventDefault();
 });
-
-//$( "div" ).click(function() {
-  // `this` is the DOM element that was clicked
-  //var index = $( "div" ).index( this );
-  //$( "span" ).text( "That was div index #" + index );
-//});
